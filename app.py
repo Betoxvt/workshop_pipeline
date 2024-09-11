c4ab14fb4da8ff01d7a6e79ec551a2c8c1e0e902
Roberto Guedes <robertoguedes@edu.univali.br>
2024-09-10 21:52:35 -0300

    primeira aula, precisa revisar

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from contrato import Vendas
+import contrato
+from datetime import datetime, time
+from pydantic import ValidationError
+
+def main():
+    st.title('Sistema de CRM e Vendas da ZapFlow - Frontend Simples')
+    email = st.text_input('Campo de texto para inserção do email do vendedor')
+    data = st.date_input('Data da compra', datetime.now())
+    hora = st.time_input('Hora da compra', value=time(9,0))
+    valor = st.number_input('Valor da venda', min_value=0.0, format='%.2f')
+    quantidade = st.number_input('Quantidade de produtos')
+    produto = st.selectbox('Produto', options=['ZapFlow com Gemini', 'ZapFlow com ChatGPT', 'ZapFlow com Llama3.0'])
+
+    if st.button('Salvar'):
+        try:
+            data_hora = datetime.combine(data, hora)
+            venda = Vendas(
+                email = email,
+                data = data_hora,
+                valor = valor,
+                quantidade = quantidade,
+                produto = produto
+            )
+            st.write(venda)
+        except ValidationError as e:
+            st.error(f'Erro: {e}')
+
+
+if __name__ == '__main__':
+    main()
